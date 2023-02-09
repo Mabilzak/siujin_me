59c4328a0ab1635a57cbf0d587a5870ee4492acb
Mabilzak <mabilzak@gmail.com>
2023-02-09 17:32:23 +0700

    Penyelesaian frond end halaman ujian_siswa.html

--- style/script_ujian_siswa.js
+++ style/script_ujian_siswa.js
@@ -8,3 +8,20 @@ classNavbar[0].addEventListener("click", function () {
   classNavbar[0].classList.toggle("navbar-click");
   classSoljab[0].classList.toggle("place_full");
 });
+
+//Sweetalert2 11.2.7
+// Button 'finish-main'
+const classFinish = document.getElementsByClassName("finish-main");
+classFinish[0].addEventListener("click", function () {
+  Swal.fire({
+    icon: "warning",
+    iconColor: "#E10000",
+    title: "Apakah Kamu Yakin",
+    text: "Anda hanya bisa melakukannya satu, tidak dapat mengulang ujian lagi.",
+    showCancelButton: true,
+    confirmButtonText: "Lanjut",
+    confirmButtonColor: "#31489e",
+    cancelButtonText: "Batal",
+    cancelButtonColor: "#E10000",
+  });
+});
